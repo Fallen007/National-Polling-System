844cb249600c5bd36cb39ea13193281eb855ccb6
Aditya Halder <alphacoder121@gmail.com>
2021-09-09 23:24:51 +0530

    Uploaded Initial files

---
+++ src/main/java/com/national/polling/system/NationalPollingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.national.polling.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalPollingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalPollingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/national/polling/system/NationalPollingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.national.polling.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalPollingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
